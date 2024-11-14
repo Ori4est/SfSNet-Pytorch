a299bdc7784c22756404b0458b78d9e985bca76e
Ori4est <yang.mi@thesea.ai>
2024-11-14 13:43:47 +0900

    Update SfSNet_test.py

--- SfSNet_test.py
+++ SfSNet_test.py
@@ -1,9 +1,20 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function
 import glob
-import os
+import os, sys
 import numpy as np
 import cv2
+ci_build_and_not_headless = False
+try:
+    from cv2.version import ci_build, headless
+    ci_and_not_headless = ci_build and not headless
+except:
+    pass
+if sys.platform.startswith("linux") and ci_and_not_headless:
+    os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+if sys.platform.startswith("linux") and ci_and_not_headless:
+    os.environ.pop("QT_QPA_FONTDIR")
+    
 import torch
 from config import M, LANDMARK_PATH, PROJECT_DIR
 from src.functions import create_shading_recon
